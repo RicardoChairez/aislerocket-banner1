166c98a9eff069cb1ca48a49dcc5f7ad2f988eea
Ricardo Chairez <rdc257@nau.edu>
2022-05-26 19:35:36 -0700

    Project

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  content: [
+    './public/**/*.{html,js}',
+  ],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'purple': '#3f3cbb',
+      'midnight': '#121063',
+      'metal': '#565584',
+      'tahiti': '#3ab7bf',
+      'silver': '#ecebff',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca',
+      'maroon': '#9B132B',
+    },
+    extend: {
+      fontFamily:{
+        body: ['Roboto']
+      }
+    },
+  },
+  plugins: [],
+}
